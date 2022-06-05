46ec36d7180a3419cefb8f7cfe1cf9fac9ccdf75
The Wizard <91921167+Quinn-ERC721@users.noreply.github.com>
2022-06-04 23:51:58 -0700

    Update/cleanup for Certik official final audit
    Line 10: ORBIT -> ORBITTECH (is this showing at opensea.io/collection/ORBITTECH?)
    Line 12: 20 -> 3,333
    Line 14: price -> .2 & maxMintPerTx -> 1
    Line 18: price -> .2 & maxMintPerTx -> 1
    Line 22: price -> .2 & maxMintPerTx -> 1

--- digi-erc721-main-yarn/smart-contract/config/CollectionConfig.ts
+++ digi-erc721-main-yarn/smart-contract/config/CollectionConfig.ts
@@ -7,20 +7,20 @@ const CollectionConfig: CollectionConfigInterface = {
   // Please DO NOT change it manually!
   contractName: 'YourNftToken',
   tokenName: 'Particle Accelerator',
-  tokenSymbol: 'ORBIT',
+  tokenSymbol: 'ORBITTECH',
   hiddenMetadataUri: 'ipfs://QmbnGdBS354HhCBzZV62rg8qgCiqxFofSqd8PAJrmX39BP/1.json',
-  maxSupply: 20,
+  maxSupply: 3,333,
   whitelistSale: {
-    price: 0.05,
+    price: 0.20,
     maxMintAmountPerTx: 1,
   },
   preSale: {
-    price: 0.07,
-    maxMintAmountPerTx: 2,
+    price: 0.20,
+    maxMintAmountPerTx: 1,
   },
   publicSale: {
-    price: 0.01,
-    maxMintAmountPerTx: 5,
+    price: 0.20,
+    maxMintAmountPerTx: 1,
   },
   contractAddress: 'null',
   openSeaSlug: 'my-nft-token',
